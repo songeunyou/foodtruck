6100b09d6a8534951d4a9a26ac3c265942c84c4f
song <songeun.you7@gmail.com>
2022-02-16 18:08:47 -0500

    Turn current truck filtering into function

--- foodtruck/pages/layouts/CurrentTrucks.jsx
+++ foodtruck/pages/layouts/CurrentTrucks.jsx
@@ -10,14 +10,20 @@ export default function CurrentTrucks({ nearbyTrucks }) {
   const date = new Date()
   const currHour = date.getHours()
 
-  let currentTrucks = nearbyTrucks.filter(truck => {
-    let startTime = parseInt(truck.data.start24.split(":")[0])
-    let endTime = parseInt(truck.data.end24.split(":")[0])
+  useEffect(() => {
+    getCurrentTrucks()
+  }, [getCurrentTrucks, nearbyTrucks])
 
-    // return true
-    // FOR DEMO PURPOSES we won't use the current time
-    return startTime <= currHour && endTime > currHour
-  })
+  function getCurrentTrucks() {
+    let currentTrucks = nearbyTrucks.filter(truck => {
+      let startTime = parseInt(truck.data.start24.split(":")[0])
+      let endTime = parseInt(truck.data.end24.split(":")[0])
+
+      // return true
+      // FOR DEMO PURPOSES we won't use the current time
+      return startTime <= currHour && endTime > currHour
+    })
+  }
 
   return (
     <div className={ftStyles.currentTrucksList}>
