7adc6bdee92052ff8a49bcc8ef4ae4584c9a80c9
song <songeun.you7@gmail.com>
2022-02-16 03:45:38 -0500

    Get list of nearby food trucks

---
+++ foodtruck/pages/components/CurrentTrucks.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import ftStyles from '../../styles/CurrentTrucks.module.scss'
+
+export default function CurrentTrucks({ nearbyTrucks }) {
+
+  return (
+    <div className={ftStyles.container}>
+
+    </div>
+  )
+}
---
+++ foodtruck/pages/components/TruckSchedule.jsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import ftStyles from '../../styles/TruckSchedule.module.scss'
+
+export default function TruckSchedule() {
+  return (
+    <div className={ftStyles.container}>
+    </div>
+  )
+}
