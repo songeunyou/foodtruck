dbc0f78ae5cdda0c0bf47db4eae5ec18c96eaf48
song <songeun.you7@gmail.com>
2022-02-16 12:16:36 -0500

    Display trucks that are nearby

--- foodtruck/pages/components/CurrentTrucks.jsx
+++ foodtruck/pages/components/CurrentTrucks.jsx
@@ -3,33 +3,51 @@ import Head from 'next/head'
 import Image from 'next/image'
 import ftStyles from '../../styles/CurrentTrucks.module.scss'
 
-function InfoCard({ distance, applicant, optionaltext }) {
+function InfoCard({ distance, applicant, optionaltext, location }) {
+  let locQuery = location.split(" ").join("+") + "+San+Francisco"
+
   return (
-    <div>
+    <div className={ftStyles.infoCard}>
       <div>
         <h3>{applicant}</h3>
         <p>{optionaltext}</p>
       </div>
 
-      <div>
-        <p>{distance}ft away</p>
-        <a>See on Map</a>
+      <div className={ftStyles.locInfo}>
+        <p className={ftStyles.distance}>{distance}ft away</p>
+        <a
+          className={ftStyles.mapBtn}
+          href={`https://www.google.com/maps/search/?api=1&query=${locQuery}`}
+          target="blank">View on Map</a>
       </div>
     </div>
   )
 }
 
 export default function CurrentTrucks({ nearbyTrucks }) {
+  const date = new Date()
+  const currHour = date.getHours()
 
-  useEffect(() => {
+  let currentTrucks = nearbyTrucks.filter(truck => {
+    let startTime = parseInt(truck.data.start24.split(":")[0])
+    let endTime = parseInt(truck.data.end24.split(":")[0])
 
+    // return true
+    // FOR DEMO PURPOSES we won't use the current time
+    return startTime <= currHour && endTime > currHour
   })
 
   return (
-    <div className={ftStyles.container}>
-      {nearbyTrucks.map((truck, i) =>
+    <div className={ftStyles.currentTrucksList}>
+      {currentTrucks.length > 0 ?
+        currentTrucks.map((truck, i) =>
         <InfoCard key={i} distance={truck.distance} {...truck.data }/>
-      )}
+      )
+      :
+      <div className={ftStyles.noneNearby}>
+        No trucks are nearby at this time :(
+      </div>
+      }
     </div>
   )
 }
