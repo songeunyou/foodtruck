4dae6d150c69b3fab5f7dcfcbe7be8c783717a0b
song <songeun.you7@gmail.com>
2022-02-16 04:31:29 -0500

    Display truck data in info card

--- foodtruck/pages/components/CurrentTrucks.jsx
+++ foodtruck/pages/components/CurrentTrucks.jsx
@@ -1,12 +1,35 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import ftStyles from '../../styles/CurrentTrucks.module.scss'
 
+function InfoCard({ distance, applicant, optionaltext }) {
+  return (
+    <div>
+      <div>
+        <h3>{applicant}</h3>
+        <p>{optionaltext}</p>
+      </div>
+
+      <div>
+        <p>{distance}ft away</p>
+        <a>See on Map</a>
+      </div>
+    </div>
+  )
+}
+
 export default function CurrentTrucks({ nearbyTrucks }) {
+
+  useEffect(() => {
+
+  })
 
   return (
     <div className={ftStyles.container}>
-
+      {nearbyTrucks.map((truck, i) =>
+        <InfoCard key={i} distance={truck.distance} {...truck.data }/>
+      )}
     </div>
   )
 }
